82a41497f4ee4cae227af89cc8c1fd088b829569
isshin123 <isshin95@gmail.com>
2022-03-25 16:51:36 +0900

    Search frame 구현

---
+++ src/components/Search.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <input 
+      class="form-control"
+      type="text" 
+      v-model="title"
+      placeholder="Search for Movies, Series and more" />
+    <div class="selects">
+      <select
+        v-for="filter in filters"
+        v-model="$data[filter.name]"                  
+        :key="filter.name"
+        class="form-select">
+        <option
+          v-if="filter.name ==='year'" 
+          value="">
+          All Years
+        </option> <!-- value값이 없는 option은 text content가 그 자체 값이 되고 value값이 있다면 value="" 값을 따라간다. value="" + text content는 textcontent가 가시적으로 표시되지만 실제 값은 ""이다-->
+        <option
+          v-for="item in filter.items"
+          :key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data(){
+    return{
+      title: '',
+      type: 'movie',
+      number: 10,
+      year: '',                               // 아무것도 선택되지 않은 것으로 취급
+      filters: [
+        {
+          name:'type',
+          items:['movie','series','episode']
+        },
+        {
+          name: 'number',
+          items: [10,20,30]
+        },
+        {
+          name: 'year',
+          items: (() => {
+            const years = []
+            const thisYear = new Date().getFullYear()
+            for (let i = thisYear; i>=1985; i--) {
+              years.push(i);
+            }
+            return years
+          })()               // 즉시실행 함수
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  display: flex;
+  > * {
+    margin-right: 10px;
+    font-size:15px;
+    &:last-child{
+      margin-right: 0;
+    }
+  }
+  .selects{
+    display: flex;
+    select{
+      width: 120px;
+      margin-right: 10px;
+      &:last-child{
+        margin-right: 0;
+      }
+    }
+  }
+}
+</style>
