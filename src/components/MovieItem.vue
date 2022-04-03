afb041af935efae0bf73f530dc1e767262a77824
isshin123 <isshin95@gmail.com>
2022-04-03 19:37:50 +0900

    영화검색 시 시각적 리스트 표현

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>{{ movie.Title }}</div>
+  <div
+    class="movie"
+    :style="{ backgroundImage: `url(${movie.Poster})` }">
+    <div class="info">
+      <div class="year">
+        {{ movie.Year }}
+      </div>
+      <div class="title">
+        {{ movie.Title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +18,53 @@ export default {
   props:{
     movie: {
       type: Object,
-      default: () => ({})     // Object타입은 default가 함수여야함
+      default: () => ({})   // Object타입은 default가 함수여야함
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.movie {
+  position: relative;
+  $width: 200px;
+  width: $width;
+  height: $width * (3/2);
+  margin: 10px;
+  border-radius: 4px;
+  background-color: $gray-400;
+  background-size: cover;         // 배경이미지의 너비를 요소의 더 넓은값에 맞춰서 출력
+  overflow: hidden;
+  &:hover::after{
+    content:"";
+    position:absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border:6px solid $primary;
+  }
+  .info{
+    background-color: rgba($black, .3);
+    width: 100%;
+    padding: 14px;
+    font-size: 14px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    text-align: center;
+    backdrop-filter: blur(10px);
+    .year{
+      color: $primary;
+    }
+    .title{
+      color: $white;
+      white-space:nowrap;
+      overflow:hidden;
+      text-overflow: ellipsis;
+    }
+  }
+}
+</style>
